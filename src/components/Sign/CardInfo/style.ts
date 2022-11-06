fa363736ec3b3ec7f980d9a13e12ffbf8f3e77d6
Thiago Galvao <thiagogalvao@pop-os.localdomain>
2022-11-06 15:48:33 -0300

    added effect in card hover

--- src/components/Sign/CardInfo/style.ts
+++ src/components/Sign/CardInfo/style.ts
@@ -13,10 +13,19 @@ export const Container = styled.div`
 	border-radius: 14px;
 
 	background-color: ${colors.background};
+	transition: all 0.1s ease-in-out;
 
 	-webkit-box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.3);
 	-moz-box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.3);
 	box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.3);
+
+	&:hover {
+		transform: scale(1.02);
+		p {
+			color: ${colors.grayLight};
+			border-bottom: 3px solid ${colors.blueLight};
+		}
+	}
 
 	svg {
 		font-size: 2rem;
@@ -28,5 +37,8 @@ export const Container = styled.div`
 		font-size: 0.95rem;
 		color: ${colors.grayMedium};
 		text-align: center;
+		transition: all 0.1s ease-in-out;
+		padding-bottom: .5rem;
+
 	}
 `;
